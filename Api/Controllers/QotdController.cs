9244c1a9012b8dcb9243d51edf0dc720756dc8b0
ailci <ali.ilci@gmx.de>
2025-04-01 14:52:47 +0200

    Add Domain and Shared projects; implement QotdController
    Updated project structure to include new `Domain` and `Shared` projects for better organization. Added `QotdController` in the `Api` project with an endpoint for "Quote of the Day". Created `Author` and `Quote` entity classes in the `Domain` project. Established `QuoteOfTheDayDto` in the `Shared` project for data transfer. Both new projects target .NET 9.0 with implicit usings and nullable reference types enabled.

---
+++ Api/Controllers/QotdController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DataTransferObjects;
+
+namespace Api.Controllers;
+
+[Route("api/qotd")]   // => localhost:7050/api/qotd
+[ApiController]
+public class QotdController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetQuoteOfTheDay() // => localhost:7050/api/qotd
+    {
+        var qotd = new QuoteOfTheDayDto
+        {
+            AuthorName = "Ich",
+            AuthorDescription = "Dozent",
+            AuthorBirthDate = new DateOnly(1978, 07, 13),
+            QuoteText = "Larum Lierum"
+        };
+
+        return Ok(qotd);
+    }
+}
