7be0bf31b3be87b9c73604c8c3604d76d639b61f
ailci <ali.ilci@gmx.de>
2025-04-02 13:42:34 +0200

    Add properties to AuthorDto for enhanced data handling
    This commit introduces several new properties to the `AuthorDto` class, including `Id`, `Name`, `Description`, `BirthDate`, `Photo`, and `PhotoMimeType`. The `System.ComponentModel.DataAnnotations` namespace is also added to facilitate potential data validation in the future.

--- Shared/DataTransferObjects/AuthorDto.cs
+++ Shared/DataTransferObjects/AuthorDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,4 +9,10 @@ namespace Shared.DataTransferObjects;
 
 public class AuthorDto
 {
+    public Guid Id { get; init; }
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+    public DateOnly? BirthDate { get; init; }
+    public byte[]? Photo { get; init; }
+    public string? PhotoMimeType { get; init; }
 }
