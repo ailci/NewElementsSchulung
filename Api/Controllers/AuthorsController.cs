e2423de05c8d55a6928232ce565ad8600f436bf3
ailci <ali.ilci@gmx.de>
2025-04-02 14:21:57 +0200

    AuthorsController

---
+++ Api/Controllers/AuthorsController.cs
@@ -0,0 +1,38 @@
+﻿using Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthorsController : ControllerBase
+{
+    #region Members/Constructors
+
+    private readonly IServiceManager _serviceManager;
+    private readonly ILoggerManager _logger;
+
+    public AuthorsController(IServiceManager serviceManager, ILoggerManager logger)
+    {
+        _serviceManager = serviceManager;
+        _logger = logger;
+    }
+
+    #endregion
+
+    #region GET
+
+    [HttpGet]
+    public async Task<IActionResult> GetAuthors()
+    {
+        _logger.LogInformation($"{nameof(GetAuthors)} aufgerufen...");
+
+        var authorDtos = await _serviceManager.AuthorService.GetAuthorsAsync();
+
+        return Ok(authorDtos);
+    }
+
+    #endregion
+}
