9f67d25f54ea96777b27bcad0a21be9dcd995d88
ailci <ali.ilci@gmx.de>
2025-04-02 16:57:45 +0200

    Enable sensitive data logging for QotdContext
    This commit adds a line to the `AddDbContext` method
    to enable sensitive data logging in the Entity Framework
    Core logs. This change aids in debugging but should
    be used with caution in production environments.

--- Api/Extensions/ServiceExtensions.cs
+++ Api/Extensions/ServiceExtensions.cs
@@ -33,6 +33,7 @@ public static class ServiceExtensions
         services.AddDbContext<QotdContext>(options =>
         {
             options.UseSqlServer(connectionString);
+            options.EnableSensitiveDataLogging();
         });
 
         return services;
