0ae73d7cbe468442a80780598ac159929a36b9de
ailci <ali.ilci@gmx.de>
2025-04-04 11:52:23 +0200

    Qotd

---
+++ Client.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shared.DataTransferObjects;
+
+namespace Client.UI.Pages;
+
+public class IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory) : PageModel
+{
+    public QuoteOfTheDayDto? QotdDto { get; set; }
+
+    public async Task OnGetAsync()
+    {
+        try
+        {
+            //1. Version Klassik
+            //var client = httpClientFactory.CreateClient("qotdapiservice");
+            //var response = await client.GetAsync("api/qotd");  // BaseAddress + Individuell => localhost:7050/api/qotd
+            
+            //response.EnsureSuccessStatusCode();
+            //var content = await response.Content.ReadAsStringAsync();
+            //QotdDto = JsonSerializer.Deserialize<QuoteOfTheDayDto>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+
+            //2. Version Abkürzung
+            var client = httpClientFactory.CreateClient("qotdapiservice");
+            QotdDto = await client.GetFromJsonAsync<QuoteOfTheDayDto>("api/qotd");
+        }
+        catch (HttpRequestException ex)
+        {
+           logger.LogError($"StatusCode: {ex.StatusCode} Message: {ex.Message}");
+        }
+    }
+}
